5c38290af61637c1b364c55ed706f123a001de8b
Ryan Kramer <RKramer613@gmail.com>
2017-06-19 16:49:11 -0700

    Added first prompt

--- adventure.js
+++ adventure.js
@@ -4,3 +4,19 @@
 setTimeout(function() {
     window.confirm("Shall we begin?");
 }, 1000);
+
+// Ask the player their age
+setTimeout(function() {
+
+    var age = prompt("How many years do you possess?");
+    if(age < 13) {
+        alert("You shall not pass!");
+    } else {
+        alert("You may proceed, but proceed at your own risk.");
+    }
+
+    
+
+
+
+}, 3000);
