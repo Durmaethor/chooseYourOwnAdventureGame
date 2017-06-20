e4067e95079b7fa6624411657ef028b1e314ce97
Ryan Kramer <RKramer613@gmail.com>
2017-06-19 17:10:53 -0700

    First rendition of choice1 with an incorrect choice response. Needs fixing

--- adventure.js
+++ adventure.js
@@ -9,13 +9,31 @@ setTimeout(function() {
 setTimeout(function() {
 
     var age = prompt("How many years do you possess?");
-    if(age < 13) {
-        alert("You shall not pass!");
-    } else {
-        alert("You may proceed, but proceed at your own risk.");
-    }
 
-    
+        if(age < 13) {
+            alert("You shall not pass!");
+        } else {
+            alert("You may proceed, but proceed at your own risk.");
+        }
+
+    alert("You wake up suddenly with a pounding head ache. You have no clue where you are, or what happened.");
+
+    var choice1 = prompt("Do you open your eyes? Or do you wait and listen?");
+
+        if(choice1 === "open") {
+            alert("You open your eyes and see a snarling orc looking monster staring at you. He screams in rage, lifts his axe and swings it down on you. The world goes dark... Game Over.");
+        } 
+        else if(choice1 === "wait") {
+                alert("You decide to keep your eyes closed and listen to what surrounds you. You hear snarling and growling noises, as well as heavy footsteps very close. You decide it is better to play dead for a bit until whatever it is leaves.")
+        } 
+        else if(choice1 != "open" && choice1 != "wait") {
+                alert("You sit still and take a moment to choose...");
+                var choice1 = prompt("Do you open your eyes? Or do you wait and listen?");
+        }
+
+        alert(choice1);
+
+
 
 
 
