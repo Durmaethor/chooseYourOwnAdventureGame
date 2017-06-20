6cacdce41bd7b93cb78cd83e1f5ce65bf20099fd
Ryan Kramer <RKramer613@gmail.com>
2017-06-19 17:23:32 -0700

    Fixed issues

--- adventure.js
+++ adventure.js
@@ -18,20 +18,18 @@ setTimeout(function() {
 
     alert("You wake up suddenly with a pounding head ache. You have no clue where you are, or what happened.");
 
-    var choice1 = prompt("Do you open your eyes? Or do you wait and listen?");
+    var choice1 = prompt("Do you OPEN your eyes? Or do you WAIT and listen?").toUpperCase();
 
-        if(choice1 === "open") {
+        if(choice1 === "OPEN") {
             alert("You open your eyes and see a snarling orc looking monster staring at you. He screams in rage, lifts his axe and swings it down on you. The world goes dark... Game Over.");
         } 
-        else if(choice1 === "wait") {
+        else if(choice1 === "WAIT") {
                 alert("You decide to keep your eyes closed and listen to what surrounds you. You hear snarling and growling noises, as well as heavy footsteps very close. You decide it is better to play dead for a bit until whatever it is leaves.")
         } 
-        else if(choice1 != "open" && choice1 != "wait") {
-                alert("You sit still and take a moment to choose...");
-                var choice1 = prompt("Do you open your eyes? Or do you wait and listen?");
+        else if(choice1 != "OPEN" && choice1 != "WAIT") {
+                alert("You sit still and take a moment to choose... refresh the page and make a choice!");
         }
 
-        alert(choice1);
 
 
 
